10e635d9605dbd3c875f75dca3c9c27ea79c38e1
Casey MacPherson <casey.macpherson@gettyimages.com>
2016-01-16 10:44:01 -0800

    Now brings in brain.coffee from hubot source. Uses in memory adapter, so no need for redis or other persistence requirements, hubot is now a dev dependency

--- test/brain.js
+++ test/brain.js
@@ -1,14 +1,15 @@
 require('coffee-script')
 require('coffee-script/register')
+
 var MockRobot = require('../'),
     robotCtx = new MockRobot(),
     expect = require('chai').expect
+
+//Calls brain.set
+var testScript = require('./testscripts/braintest.coffee')(robotCtx)
     
 describe('Mock should capture brain interactions',function() {
-  before(function(done) {
-    
-    done()
+  it('Sets the testkey and value',function() {
+    expect(robotCtx.brain.get('TestKey')).to.be.equal('TestValue')
   })
-  
-  it('')
 })
---
+++ test/testscripts/braintest.coffee
@@ -0,0 +1,2 @@
+module.exports = (robot) ->
+  robot.brain.set('TestKey','TestValue')
